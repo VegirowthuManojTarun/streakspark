7239bb79eada042410625b9dc4ea50bf61f255b9
tasklabs994 <manojtarun934@gmail.com>
2025-06-18 15:29:41 +0530

    vercel.config added

--- server/utils/googleConfig.js
+++ server/utils/googleConfig.js
@@ -2,9 +2,9 @@ const { google } = require("googleapis");
 
 const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-
+const GOOGLE_CALLBACK_URL = process.env.GOOGLE_CALLBACK_URL;
 exports.oath2client = new google.auth.OAuth2(
   GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET,
-  "postmessage" // This is the redirect URI for OAuth2
+  GOOGLE_CALLBACK_URL
 );
